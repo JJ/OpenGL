cc773b4cde49b5b6f93455d78cf644e6ae35feb8
rafa <rafaelleru95103@gmail.com>
2016-07-27 12:00:27 +0200

    Terminada cabecera

--- Practica2-PLY_y_Revolucion/figure.h
+++ Practica2-PLY_y_Revolucion/figure.h
@@ -15,16 +15,26 @@ class Figura{
   vector<_vertex3f> vertices;
   vector<_vertex3i> caras;
   vector<_vertex2i> aristas;
+  int long_perfil;
 
+  void calculaTapas(int axis, int ngiros);
+  bool checkTapas();
+  void rotateX(int ncaras);
+  void rotateY(int ncaras);
+  void rotateZ(int ncaras);
+  //void calculeNewCoord(float& a_1, float& b_1, const float a, const float b,  double alfa);
  public:
   Figura(){};
   Figura(char* ply_name); //Constructor a partir de un ply;
   void init(vector<_vertex3f> vertices, vector<_vertex3i> caras);
   Figura& operator = (Figura &asig);
   void initfromPLY(char* ply_name);
+  void rotate(int ncaras = 36);
   void pointsMode();
   void linesMode();
   void solidMode(bool chessMode);
+
+  void rotate();
 };
 
 #endif
